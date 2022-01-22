225a1c973b74fec14b0010c4b58ca8f10bf5e77f
makeshaart <makeshaart@gmail.com>
2022-01-22 18:44:15 +0300

    test task

--- test_task/index.js
+++ test_task/index.js
@@ -1,86 +1,84 @@
-    // let arr=[2, 4, 5 ,6];
+    let arr=[2, 4, 5 ,6];
 
     // create new array with map
 
-    // let nar = arr.map((a) => a*2 )
-    // console.log(nar);
+    let nar = arr.map((a) => a*2 )
+    console.log(nar);
 
 
     // create new array with forEach
 
-    // let nar=[];
-    // arr.forEach((value, index) =>
-    // {nar[index] = arr[index]* 2})
-    // console.log(nar);
+    let nar=[];
+    arr.forEach((value, index) =>
+    {nar[index] = arr[index]* 2})
+    console.log(nar);
 
 
     // create new array with for
 
-//     let nar=[];
-//     for(i=0; i<arr.length; i++){
-//         nar[i]=arr[i]*2;        
-//    }
-    // console.log(nar);
+    let nar=[];
+    for(i=0; i<arr.length; i++){
+        nar[i]=arr[i]*2;        
+   }
+    console.log(nar);
 
 
     // create new array with for...of
 
-    // let nar=[];
-    // let i=0;
-    // for (let number of arr) {
-    //     nar[i]=number*2;
-    //     i++;
-    // }
-    // console.log(nar);
+    let nar=[];
+    let i=0;
+    for (let number of arr) {
+        nar[i]=number*2;
+        i++;
+    }
+    console.log(nar);
 
     //PRIME TASKS
 
-// let n = prompt("Type number here", "");
-// const primes = [];
-// for (let i = 2; i <= n; i++) {
-//   for (let j = 2; j <= n; j++) {
-//     if ((i % j == 0) && (j != i)) {
-//         break;
-//     } else {
-//         primes.push(i)
-//         break;
-//     }
-//   }
-// }
-// console.log(primes);
+let n = prompt("Type number here", "");
+const primes = [];
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j <= n; j++) {
+    if ((i % j == 0) && (j != i)) {
+        break;
+    } else {
+        primes.push(i)
+        break;
+    }
+  }
+}
+console.log(primes);
 
-// let n = prompt("Type number here", "");
-// for (let i = 2; i <= n; i++) {
-//     if ((n % i == 0) && (n != i)) {
-//         break;
-//     } else {
-//         console.log(n,'it`s a prime num')
-//         break;
-//     }
-// }
-
-
+let n = prompt("Type number here", "");
+for (let i = 2; i <= n; i++) {
+    if ((n % i == 0) && (n != i)) {
+        break;
+    } else {
+        console.log(n,'it`s a prime num')
+        break;
+    }
+}
 
 
-//  let number = prompt("Type number here", "");
-//     if((number%2===0) || (number%3===0)|| (number%5===0)|| (number%7===0)){
-//         console.log(number,'not a prime');
-//     }else{
-//         console.log(number,'it`s a prime num');
-//     }
-// 
+ let number = prompt("Type number here", "");
+    if((number%2===0) || (number%3===0)|| (number%5===0)|| (number%7===0)){
+        console.log(number,'not a prime');
+    }else{
+        console.log(number,'it`s a prime num');
+    }
 
 
-// let number = prompt("Type number here", "");
-// const seive = [];
-// const primes = [];
-// for( let i=2; i<number; i++){
-//     if(!seive[i]){
-//         primes.push(i);
-//         for(let j = i*i; j<number; j+=i){
-//             seive[j]=true;
-//         }
-//     }
-// }
-// console.log(primes);
+
+let number = prompt("Type number here", "");
+const seive = [];
+const primes = [];
+for( let i=2; i<number; i++){
+    if(!seive[i]){
+        primes.push(i);
+        for(let j = i*i; j<number; j+=i){
+            seive[j]=true;
+        }
+    }
+}
+console.log(primes);
 
